624bc55f841f639f4c9cf68dd02b1955a8497fd1
eugen <anghel.bogdan.eugen@gmail.com>
2019-10-24 19:07:01 +0300

    update

--- VeveOfTanks/veveOfTanks.py
+++ VeveOfTanks/veveOfTanks.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 
 # Initialise screen
@@ -9,42 +10,66 @@ pygame.display.set_caption('Veve Of Tanks')
 icon = pygame.image.load('VeveOfTanks\iconTank.PNG')
 pygame.display.set_icon(icon)
 
-# PlayerImage
+# Backgrownd
+background = pygame.image.load("VeveOfTanks\desertTexture.jpg")
+
+# Player
 playerImg = pygame.image.load("VeveOfTanks\\tank.png")
 playerX = 370
 playerY = 480
+
 # player move
 playerX_change = 0
 playerY_change = 0
 
+# Enemy
+enemyImg = pygame.image.load("VeveOfTanks\\enemyTank.png")
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 160)
+
+# enemy move
+enemyX_change = 2
+enemyY_change = 30
+
 def player(x,y):
     screen.blit(playerImg, (x, y))
+
+def enemy(x,y):
+    screen.blit(enemyImg, (x, y))
 
 # Game loop 
 running = True
 while running:
     screen.fill((0, 0, 0))
+    # background image
+    screen.blit(background, (0,0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         # Move tank left and right
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.3
+                playerX_change = -2
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.3
+                playerX_change = 2
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
         # Move tank up and down
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                playerY_change = -0.3
+                playerY_change = -2
             if event.key == pygame.K_DOWN:
-                playerY_change = 0.3
+                playerY_change = 2
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerY_change = 0
+
+    # Player move in loop
+    playerX += playerX_change
+    playerY += playerY_change    
+    
     # Set boundry left/right
     if playerX  <= 0:
         playerX = 0
@@ -55,8 +80,18 @@ while running:
         playerY = 0
     elif playerY >= 536:
         playerY = 536
-    # Player move in loop
-    playerX += playerX_change
-    playerY += playerY_change
+
+    # Enemy move in loop
+    enemyX += enemyX_change    
+
+    # Set enemy boundry left/right
+    if enemyX  <= 0:
+        enemyX_change =+ 2
+        enemyY += enemyY_change
+    elif enemyX >= 736:
+        enemyX_change =-2 
+        enemyY += enemyY_change    
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
