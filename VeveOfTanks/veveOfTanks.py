4f43d99c6b950521ec70bbedb5af2cce21fd04ea
eugen <anghel.bogdan.eugen@gmail.com>
2019-10-26 13:56:48 +0300

    update

--- VeveOfTanks/veveOfTanks.py
+++ VeveOfTanks/veveOfTanks.py
@@ -31,11 +31,24 @@ enemyY = random.randint(50, 160)
 enemyX_change = 2
 enemyY_change = 30
 
+# bullet ready
+bulletImg = pygame.image.load("VeveOfTanks\\bullet.png")
+bulletX = 0
+bulletY = playerY
+bulletY_change = 5
+bulletX_change = 0
+bullet_state = "ready"
+
 def player(x,y):
     screen.blit(playerImg, (x, y))
 
 def enemy(x,y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16,y+10))
 
 # Game loop 
 running = True
@@ -62,14 +75,21 @@ while running:
                 playerY_change = -2
             if event.key == pygame.K_DOWN:
                 playerY_change = 2
+            if event.key == pygame.K_SPACE:
+                if bullet_state is "ready":
+                    # Get the current x cordinate of the tank
+                    bulletX = playerX
+                    bulletY = playerY
+                    fire_bullet(bulletX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerY_change = 0
-
+            
     # Player move in loop
     playerX += playerX_change
     playerY += playerY_change    
-    
+           
     # Set boundry left/right
     if playerX  <= 0:
         playerX = 0
@@ -92,6 +112,16 @@ while running:
         enemyX_change =-2 
         enemyY += enemyY_change    
 
+    # Bullet Movement
+    if bulletY <= 0:
+        bulletY = playerY
+        bullet_state = "ready"
+
+    if bullet_state is "fire":
+        fire_bullet(bulletX, bulletY)
+        bulletY -= bulletY_change
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
+
     pygame.display.update()
